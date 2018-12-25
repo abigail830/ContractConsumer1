850131582e5e6c27445e63fb28210807b3c85fb3
SaraQian <abigail830@163.com>
2018-12-25 10:42:54 +0800

    init commit

---
+++ src/main/java/com/github/abigail830/ContractConsumer1/ContractConsumer1Application.java
@@ -0,0 +1,14 @@
+package com.github.abigail830.ContractConsumer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractConsumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractConsumer1Application.class, args);
+	}
+
+}
+
